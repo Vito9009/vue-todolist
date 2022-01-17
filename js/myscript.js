ce2b708687212f34a871fba7bffbc3db8c1ce553
Your Name <yan.vi193@gmail.com>
2022-01-17 15:04:13 +0100

    stampa-elementi-todo-e-inserimento-nuovi-con-keyup

--- js/myscript.js
+++ js/myscript.js
@@ -11,18 +11,53 @@ Stampare all’interno di una lista, un item per ogni todo.
 Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
 
 MILESTONE 2
-Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
+Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo 
+viene rimosso dalla lista.
 
 MILESTONE 3
-Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, 
+il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene 
+aggiunto alla lista dei todo esistenti.
 
 Bonus:
 1- oltre al click sul pulsante, intercettare anche il tasto ENTER per aggiungere il todo alla lista
-2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente (se done era uguale a false, impostare true e viceversa)
+2- cliccando sul testo dell’item, invertire il valore della proprietà done del todo corrispondente 
+(se done era uguale a false, impostare true e viceversa)
 */
-let app = new Vue({
-    el: '',
-    data:{
-        
+var app = new Vue({
+    el: '#myvuecontainer',
+    data: {
+      newtodotext: "",
+      todo: [
+          {
+            todotext: 'Creare lista To Do',
+            tododone: 'true'
+          },
+          {
+            todotext: 'Pulire casa',
+            tododone: 'false'
+          },
+          {
+            todotext: 'Fare la spesa',
+            tododone: 'false'
+          },
+          {
+            todotext: 'Guardare serie TV',
+            tododone: 'false'
+          }
+      ]
+    },
+
+    methods: {
+        add: function(){
+            this.todo.push(
+            {
+                todotext: this.newtodotext,
+                tododone: 'false'
+            }
+            );
+            this.newtodotext = "";
+            console.log(this.todo);
+        }
     }
-});
+  })
